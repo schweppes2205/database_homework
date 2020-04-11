3bfb576597f4d1cbe78602e5e51577f52cc48f48
user01 <schweppes2205@gmail.com>
2020-04-11 21:31:54 +0300

    lecture03. task01

---
+++ lecture03/01.sql
@@ -0,0 +1,51 @@
+-- Ќаписать крипт, добавл€ющий в Ѕƒ vk, которую создали на зан€тии, 3 новые таблицы (с перечнем полей, указанием индексов и внешних ключей)
+-- таблица news - новости, которые может публиковать пользователь. может вставл€ть туда медиа.
+DROP TABLE IF EXISTS news;
+CREATE TABLE news (
+	id SERIAL PRIMARY KEY,
+	user_id BIGINT UNSIGNED NOT NULL,
+	media_id BIGINT UNSIGNED NOT NULL,
+	content text,
+	
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (media_id) references media(id)
+);
+
+-- таблица items - магазин услуг.
+
+drop table if exists items;
+create table items(
+	id serial primary key,
+	content text,
+
+	FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- таблица users_items - св€зь ћ х ћ элементов каталога и пользователей.
+
+drop table if exists users_items;
+create table users_items(
+	user_id bigint unsigned not null,
+	item_id bigint unsigned not null,
+	
+	primary key (user_id, item_id),
+	foreign key (user_id) references users(id),
+	foreign key (item_id) references items(id)
+);
+
+-- таблица permissions - доступ к публикаци€ми 
+
+drop table if exists permissions;
+create table permissions(
+	owner bigint unsigned not null,
+	news_permitted_user_id bigint unsigned,
+	news_id bigint unsigned,
+	items_permitted_user_id bigint unsigned,
+	items_id bigint unsigned,
+	
+	foreign key (owner) references users(id),
+	foreign key (news_permitted_user_id) references users(id),
+	foreign key (items_permitted_user_id) references users(id),
+	foreign key (news_id) references news(id),
+	foreign key (items_id) references items(id)
+);
